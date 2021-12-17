d03748d6d9f94753a324d7118abd10eb438282c7
Vevo-tor <bertasiustauras@gmail.com>
2021-12-17 19:44:56 +0200

    Keypad difficulty: Easy
    Keypad Difficulty: Easy with Cypress

--- cypress/integration/keypad.spec.js
+++ cypress/integration/keypad.spec.js
@@ -1,5 +1,13 @@
-describe('', () => {
-    it('', () => {
+describe('Keypad Difficulty: easy', () => {
+    it('Should be easy', () => {
 
+        cy.visit(Cypress.config('baseUrl'));
+        cy.get('.item').contains("Keypad - easy", { matchCase: false }).click();
+        cy.get('#todo').invoke('val').then(toDo => {
+            toDo.split('').map(numb => {
+                cy.get('#keypad > div').contains(numb).click();
+            });
+        });
+        cy.get('#submit').click();
     });
 });
