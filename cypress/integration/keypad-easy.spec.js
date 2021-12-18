7702fc88c25fee0bc41f1039d3022cb70e9719c9
Vevo-tor <bertasiustauras@gmail.com>
2021-12-18 02:05:23 +0200

    Comments and Assertions

--- cypress/integration/keypad-easy.spec.js
+++ cypress/integration/keypad-easy.spec.js
@@ -3,11 +3,16 @@ describe('Keypad Difficulty: easy', () => {
 
         cy.visit(Cypress.config('baseUrl'));
         cy.get('.item').contains("Keypad - easy", { matchCase: false }).click();
+
+        // Read Todo and fill input
         cy.get('#todo').invoke('val').then(toDo => {
             toDo.split('').map(numb => {
                 cy.get('#keypad > div').contains(numb).click();
             });
         });
+
         cy.get('#submit').click();
+        cy.get('#msg').should('exist')
+            .should('contain', 'Correct!');
     });
 });
--- cypress/integration/keypad-hard.spec.js
+++ cypress/integration/keypad-hard.spec.js
@@ -1,10 +1,15 @@
 describe('Keypad Difficulty: hard', () => {
     it('Should be hard', () => {
-        const arr = [];
-        cy.visit(Cypress.config('baseUrl'));
 
+        // Array to store unkown key values
+        const arr = [];
+
+        //baseUrl is behaving oddly
+        cy.visit(Cypress.config('baseUrl'));
         cy.get('.item').contains("Keypad - hard", { matchCase: false }).click();
-        for (let i = 1; i <= divCount; i++) {
+
+        // Unkown key values stored in arr
+        for (let i = 1; i <= 12; i++) {
             cy.get(`#keypad > :nth-child(${i})`).click();
             cy.get('#input').invoke('val').then(a => {
                 arr.push(a);
@@ -12,12 +17,16 @@ describe('Keypad Difficulty: hard', () => {
             })
         }
 
-        console.log(arr);
+        // Filling ToDo value to Input
         cy.get('#todo').invoke('val').then(toDo => {
             toDo.split('').map(numb => {
                 cy.get('#keypad > :nth-child(' + (arr.indexOf(numb) + 1) + ')').click();
             });
         });
+
+        //Submit form
         cy.get('#submit').click();
+        cy.get('#msg').should('exist')
+            .should('contain', 'Correct!');
     });
 });
